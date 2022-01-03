486a1f7bc36af3cf47d3a4da7647d4ee79ada86b
Nneka <nneka.dev@gmail.com>
2022-01-03 15:12:26 -0800

    Initial Commit

---
+++ MediaSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediaSearch
+//
+//  Created by Nneka Udoh on 1/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediaSearch/MediaSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaSearchApp.swift
+//  MediaSearch
+//
+//  Created by Nneka Udoh on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MediaSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
